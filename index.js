949eb506147ac4a5b6d4a16611806ba76f991522
tungnx7 <ngotung7795@gmail.com>
2021-10-13 22:48:31 +0700

    practice array methods

--- index.js
+++ index.js
@@ -58,7 +58,12 @@ var cm8 = {
 
 var CMs = [cm1, cm2, cm3, cm4, cm5, cm6, cm7, cm8];
 
-console.log(CMs[3].height);
+console.log(CMs.height);
+
+// Array method sử dụng hàm. forEach có tác dụng duyệt qua tất cả các phần tử nằm ở trong mảng
+var employee = CMs.forEach(function(cm, index) {
+    console.log(index, cm); 
+})
 
 // Example function: Tính diện tích và chu vi hình tròn với bán kính là 12
 function areaCircle (r) {
@@ -146,4 +151,263 @@ console.log(a); /**Kết quả là [3, 2] chỉ còn lại 2 phần tử sau khi
 console.log(a); /**Kết quả là [3, 2] là kết quả cuối cùng ở ví dụ shift */
 console.log(a.unshift(b)); /**Kết quả là 3 nghĩa là số lượng phần tử mới của mảng a sau khi thêm 1 phần tử từ mảng b */
 console.log(a); /**Kết quả là [array(2), 3, 2] phần tử array(2) là array của b mới được thêm vào ở vị trí đầu tiên trong mảng a */
+console.log(`========================`)
 
+// method: array.map(function() {điều kiện đầu ra mong muốn tương ứng}); tất cả các phần tử trong mảng đều sẽ bị biến đổi dựa trên điều kiện đầu ra mong muốn
+var rectangles = [
+    {
+        width: 10,
+        height: 5
+    },
+    {
+        width: 10,
+        height: 20
+    },
+    {
+        width: 4,
+        height: 16
+    }
+];
+// Dùng method map tính chu vi và diện tích của từng hình chữ nhật tương ứng
+var areaRectangles = rectangles.map(function(rectangle) {
+    return rectangle.width * rectangle.height   
+});
+console.log(areaRectangles);
+
+var perimeterRectangles = rectangles.map(function(rectangle) {
+    return (rectangle.width + rectangle.height)*2
+});
+console.log(perimeterRectangles);
+
+// array.filter(function() {điều kiện}); phương thức sẽ lọc các phần tử phù hợp với điều kiện để trả ra kết quả như mong muốn
+var numbers = [-5, -1, 2, 7, 10]
+
+var resultNumbers = numbers.filter(function(number) {
+    return number % 5 === 0
+})
+console.log(resultNumbers); /**[-5, 10] */
+
+var negativeNumbers = numbers.filter(function(number) {
+    return number < 0
+})
+console.log(negativeNumbers); /**[-5, -1] */
+
+// array.find(function(){điều kiện}); phương thức sẽ lọc ra phần tử phù hợp với điều kiện để trả kết quả và chỉ trả ra duy nhất 1 kết quả đầu tiên mà nó tìm được
+var negativeNumbers2 = numbers.find(function(number) {
+    return number < 0
+})
+console.log(negativeNumbers2); /**{-5} */
+
+// array.reduce(function(a, b) {điều kiện}); phương thức reduce sử dụng khi muốn nhận về kết quả duy nhất sau khi đã xử lý điều kiện để đạt kết quả như mong muốn
+var numbers = [1, 2, 3, 4]; /**tương đương mảng [x1, x2, x3, x4] */
+
+var total = numbers.reduce(function(a , b) {
+    return a + b
+});
+console.log(total); /**phương thức sẽ xử lý từng bước một theo cấu trúc  x1 + x2 = y1, sau đó mới tính tiếp y1 + x3 = y2 và cuối cùng là y2 + x4 = y3 = 10 là kết quả cuối cùng  */
+// Bài tập: Bạn vừa đặt hàng online trên shoppe với 3 món hàng như sau. Dùng phương thức reduce để tính tổng tiền bạn cần trả cho đơn hàng trên.
+var orders = [
+    {
+        name: 'shirt',
+        quantity: 1,
+        unitPrice: 120000
+    },
+    {
+        name: 'sock',
+        quantity: 5,
+        unitPrice: 15000
+    },
+    {
+        name: 'shoes',
+        quantity: 1,
+        unitPrice: 400000
+    },
+]
+
+var price = orders.map(function(item) {
+    return item.unitPrice*item.quantity
+})
+var total = price.reduce(function(a,b) {
+    return a + b;
+});
+console.log(total);
+
+// Bài tập thực hành. Giả sử bạn đang quản lý một gian hàng bán nước với nhiều các loại sản phẩm khác nhau chứa trong mảng products dưới đây
+var products = [
+    {
+        name: 'nước giải khát',
+        unitPrice: 12000,
+        quantity: 40,
+        category: 'soft drink'
+    },
+    {
+        name: 'bia sài gòn',
+        unitPrice: 12000,
+        quantity: 80,
+        category: 'beer'
+    },
+    {
+        name: 'bia hà nội',
+        unitPrice: 10000,
+        quantity: 70,
+        category: 'beer'
+    },
+    {
+        name: 'sữa',
+        unitPrice: 8000,
+        quantity: 100,
+        category: 'milk'
+    },
+    {
+        name: 'nước lọc',
+        unitPrice: 5000,
+        quantity: 30,
+        category: 'water'
+    },   
+];
+
+// Yêu cầu
+// 1. Filter các tên sản phẩm có chung category: beer.
+// 2. Tính tổng số tiền còn tồn lại trong kho.
+// 3. Chọn ra tên các sản phẩm có tiền hàng tồn lớn trên 800.000 đồng
+
+var beerList = products.filter(function(item) {
+    return item.category === 'beer'
+});
+console.log(beerList);
+
+var handlerBeerList = beerList.map(function(name) {
+    return {
+        name: name.name
+    }
+});
+console.log(handlerBeerList); /**Hoanf thành yêu cầu số 1 */
+
+
+var unitTotal = products.map(function(total) {
+    return total.quantity * total.unitPrice;
+});
+console.log(unitTotal);
+var total = unitTotal.reduce(function(a, b) {
+    return a + b;
+});
+console.log(total); /**Hoàn thành yêu cầu số 2 */
+
+var poodgoods = unitTotal.filter(function(totalPrice) {
+    return totalPrice >= 800000
+});
+
+// Bài tập: Cho một mảng với 3 phần tử như sau. Dùng reduce để ra được kết quả như mong muốn 
+var items = ['Tom', 'Bill', 'Kim'];
+// đầu ra cần thực hiện: '<Tom><Bill><Kim>'
+
+var result = items.reduce(function(currentValue, item){
+    return  currentValue + `<` + item + `>`
+}, `<`);
+console.log(result);
+
+/** array.soft: phương thức sắp xếp các phần tử trong mảng theo yêu cầu
+ *  công thức array.sort(function(a, b){})
+ * giá trị a < 0 -> a sẽ đứng trước b
+ * giá trị a > 0 -> a sẽ đứng sau b
+ * giá trị a = 0 -> giữ nguyên thứ tự ban đầu
+ */
+var numbers = [2, 9, 1, 4, 13];
+
+var ascendingNumbers = numbers.sort(function(a, b) {
+    // sắp xếp theo thứ tự từ nhỏ đến lớn
+    // đầu ra [1, 2, 4, 9, 13]
+    return a - b;
+});
+console.log(ascendingNumbers);
+
+var descendingNumbers = numbers.sort(function(a, b) {
+    // sắp xếp theo thứ tự từ lớn đến bé
+    // đầu ra [13, 9, 4, 2, 1]
+    return b -a;
+});
+console.log(descendingNumbers);
+
+// Ví dụ về sắp xếp các object trong mảng
+var employees = [
+    {
+        name: 'Nguyen Van A',
+        age: 19
+    },
+    {
+        name: 'Nguyen Thi B',
+        age: 20
+    },
+    {
+        name: 'Nguyen Duc C',
+        age: 18
+    },
+];
+
+var sortedEmployees = employees.sort(function(a, b){
+    // Sắp xếp theo thứ tự tăng dần theo độ tuổi
+    return a.age - b.age;
+});
+console.log(sortedEmployees);
+
+// Bài tập thực hành: Cho 1 mảng gồm 7 phần tử với 3 thuộc tính id, name, coin
+// Yêu cầu: Sắp xếp giá từ đắt phí đến miễn phí các phần tử theo tên và ngược lại
+var courses = [
+    {
+        id: 1,
+        name: 'HTML&CSS',
+        coin: 0
+    },
+    {
+        id: 2,
+        name: 'ReactJS',
+        coin: 1000
+    },
+    {
+        id: 3,
+        name: 'Java',
+        coin: 500
+    },
+    {
+        id: 4,
+        name: 'Sass',
+        coin: 300
+    },
+    {
+        id: 5,
+        name: 'JavaScript',
+        coin: 200
+    },
+    {
+        id: 6,
+        name: 'PHP',
+        coin: 400
+    },
+    {
+        id: 7,
+        name: 'VueJS',
+        coin: 700
+    },
+];
+
+var minCourses = courses.sort(function(a, b) { /**sắp xếp theo thứ tự từ miễn phí đến đắt phí nhất */
+    return a.coin - b.coin;
+});
+console.log(minCourses);
+var handerMinCourses = minCourses.map(function(item) {
+    return {
+        name: item.name
+    }
+});
+console.log(handerMinCourses);
+
+var maxCourses = courses.sort(function(a, b) { /**sắp xếp theo thứ tự từ đắt phí nhất đến miễn phí */
+    return b.coin - a.coin;
+});
+console.log(maxCourses);
+var handerMaxCourses = maxCourses.map(function(item) {
+    return {
+        name: item.name
+    }
+});
+console.log(handerMaxCourses);
