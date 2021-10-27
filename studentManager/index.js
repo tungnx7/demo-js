b61e8abdda23062d7bba6407b2ba3d83f22a4a32
tungnx7 <ngotung7795@gmail.com>
2021-10-27 20:36:54 +0700

    practice create a new student of project

--- studentManager/index.js
+++ studentManager/index.js
@@ -61,20 +61,145 @@
 
 // main();
 
-var readlienSync = require("readline-sync");
+// var readlineSync = require("readline-sync");
+// var fs = require("fs");
+
+// function main() {
+//   loadData();
+//   showMenu();
+// }
+// main();
+
+// function showMenu() {
+//   console.log("1. Show all students");
+//   console.log("2. Create new student");
+//   console.log("3. Save and Exit");
+
+//   var option = readlienSync.question(">");
+//   switch (option) {
+//     case "1":
+//       showStudents();
+//       showMenu();
+//       break;
+//     case "2":
+//       showCreateStudent();
+//       showMenu();
+//       break;
+//     case "3":
+//       saveAndExit();
+//       break;
+//     default:
+//       console.log("wrong option");
+//       showMenu();
+//       break;
+//   }
+// }
+
+// var students = [];
+
+// function loadData() {
+//   var fileContent = fs.readFileSync('./data.json', {encoding: 'utf8'});
+//   students = JSON.parse(fileContent);
+// }
+
+// function showStudents() {
+//   for (var student of students) {
+//     console.log(student.name + `:`, student.age, `years old`);
+//   }
+// }
+
+// function showCreateStudent() {
+//   var name = readlienSync.question('Name: ');
+//   var age = readlienSync.question('Age: ');
+//   var student = {
+//     name: name,
+//     age: parseInt(age),
+//   }
+//   students.push(student);
+// }
+
+// function saveAndExit() {
+//   var content = JSON.stringify(students);
+//   fs.writeFileSync('./data.json', content, {encoding: 'utf8'});
+// }
+
+// var readlineSync = require("readline-sync");
+// var fs = require("fs");
+
+// function main() {
+//   loadData();
+//   showMenu();
+// }
+// main();
+
+// function showMenu() {
+//   console.log("1. Show all students");
+//   console.log("2. Create new student");
+//   console.log("3. Save and Exit");
+
+//   var option = readlineSync.question(">");
+//   switch (option) {
+//     case "1":
+//       showStudents();
+//       showMenu();
+//       break;
+//     case "2":
+//       showCreateStudent();
+//       showMenu();
+//       break;
+//     case "3":
+//       saveAndExit();
+//       break;
+//     default:
+//       console.log("Wrong option");
+//       showMenu();
+//       break;
+//   }
+// }
+
+// var students = [];
+
+// function loadData(){
+//   var fileContent = fs.readFileSync('./data.json', {encoding: 'utf8'});
+//   students = JSON.parse(fileContent);
+// }
+
+// function showStudents() {
+//   for (var student of students) {
+//     console.log(student.name + `:`, student.age, `years old`);
+//   }
+// }
+
+// function showCreateStudent() {
+//   var name = readlineSync.question('Name: ');
+//   var age = readlineSync.question('Age: ');
+//   var student = {
+//     name: name,
+//     age: parseInt(age),
+//   }
+//   students.push(student);
+// }
+
+// function saveAndExit() {
+//   var content = JSON.stringify(students);
+//   fs.writeFileSync('./data.json', content, {encoding: 'utf8'});
+// }
+
+var readlineSync = require("readline-sync");
 var fs = require("fs");
 
 function main() {
+  loadData();
   showMenu();
 }
 main();
 
 function showMenu() {
-  console.log("1. Show all students");
+  console.log("1. Show all student");
   console.log("2. Create new student");
   console.log("3. Save and Exit");
 
-  var option = readlienSync.question(">");
+  var option = readlineSync.question(">");
   switch (option) {
     case "1":
       showStudents();
@@ -88,8 +213,36 @@ function showMenu() {
       saveAndExit();
       break;
     default:
-      console.log("wrong option");
+      console.log("Wrong option");
       showMenu();
       break;
   }
+}
+
+var students = [];
+
+function loadData() {
+  var fileContent = fs.readFileSync('./data.json', {encoding: 'utf8'});
+  students = JSON.parse(fileContent);
+}
+
+function showStudents() {
+  for (var student of students) {
+    console.log(student.name + `:`, student.age, `years old`);
+  }
+}
+
+function showCreateStudent() {
+  var name = readlineSync.question('Name: ');
+  var age = readlineSync.question('Age: ');
+  var student = {
+    name: name,
+    age: parseInt(age),
+  }
+  students.push(student);
+}
+
+function saveAndExit() {
+  var content = JSON.stringify(students);
+  fs.writeFileSync('./data.json', content, {encoding: 'utf8'});
 }
